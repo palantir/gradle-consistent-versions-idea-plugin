3ea1fee96ba2fd1af9be0e19f2c8e99ffa42ee8b
Finlay Williams <finlayw@palantir.com>
2024-09-20 14:08:14 +0100

    idea plugin reviewed

---
+++ gradle-consistent-versions-idea-plugin/src/main/java/com/palantir/gradle/versions/intellij/DependencyGroup.java
@@ -0,0 +1,65 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.gradle.versions.intellij;
+
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.psi.PsiElement;
+import com.palantir.gradle.versions.intellij.psi.VersionPropsTypes;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.immutables.value.Value;
+
+@Value.Immutable
+public abstract class DependencyGroup {
+
+    @Value.Parameter
+    public abstract List<String> parts();
+
+    public static DependencyGroup fromString(String string) {
+        return ImmutableDependencyGroup.of(Arrays.asList(string.split("\\.")));
+    }
+
+    public final String asUrlString() {
+        String url = String.join("/", parts());
+
+        if (!url.isEmpty()) {
+            url = url + "/";
+        }
+
+        return url;
+    }
+
+    public static DependencyGroup groupFromParameters(CompletionParameters parameters) {
+        PsiElement position = parameters.getPosition();
+        PsiElement currentElement = position.getPrevSibling();
+        if (currentElement == null && position.getNode().getElementType() == VersionPropsTypes.NAME_KEY) {
+            currentElement = position.getParent();
+        }
+
+        List<String> newParts = new ArrayList<>();
+        while (currentElement != null) {
+            if (currentElement.getNode().getElementType() == VersionPropsTypes.GROUP_PART) {
+                newParts.add(0, currentElement.getText());
+            } else if (currentElement.getNode().getElementType() == VersionPropsTypes.DEPENDENCY_GROUP) {
+                newParts.addAll(Arrays.asList(currentElement.getText().split("\\.")));
+            }
+            currentElement = currentElement.getPrevSibling();
+        }
+        return ImmutableDependencyGroup.of(newParts);
+    }
+}
---
+++ gradle-consistent-versions-idea-plugin/src/test/java/com/palantir/gradle/versions/intellij/VersionPropsCodeInsightTest.java
@@ -0,0 +1,70 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.palantir.gradle.versions.intellij;
+
+import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class VersionPropsCodeInsightTest extends LightJavaCodeInsightFixtureTestCase {
+
+    @Test
+    public void test_version_completion() throws Exception {
+        setUp();
+        // The file name is required for context but does not need to exist on the filesystem
+        myFixture.configureByText("versions.props", "com.palantir.baseline:baseline-error-prone = <caret>");
+        myFixture.complete(CompletionType.BASIC);
+        List<String> lookupElementStrings = myFixture.getLookupElementStrings();
+        assertNotNull(lookupElementStrings);
+        assertContainsElements(lookupElementStrings, "0.66.0", "2.40.2");
+        tearDown();
+    }
+
+    @Test
+    public void test_group_completion() throws Exception {
+        setUp();
+        // The file name is required for context but does not need to exist on the filesystem
+        myFixture.configureByText("versions.props", "com.palantir.baseline.<caret>");
+        myFixture.complete(CompletionType.BASIC);
+        List<String> lookupElementStrings = myFixture.getLookupElementStrings();
+        assertNotNull(lookupElementStrings);
+        assertContainsElements(lookupElementStrings, "baseline-error-prone", "baseline-null-away");
+        tearDown();
+    }
+
+    @Test
+    public void test_package_completion() throws Exception {
+        setUp();
+        // The file name is required for context but does not need to exist on the filesystem
+        myFixture.configureByText("versions.props", "com.palantir.baseline:<caret>");
+        myFixture.complete(CompletionType.BASIC);
+        List<String> lookupElementStrings = myFixture.getLookupElementStrings();
+        assertNotNull(lookupElementStrings);
+        assertContainsElements(lookupElementStrings, "baseline-error-prone", "baseline-null-away");
+        tearDown();
+    }
+
+    @Test
+    public void test_other_file_names() throws Exception {
+        setUp();
+        myFixture.configureByText("notVersions.props", "com.palantir.baseline:<caret>");
+        myFixture.complete(CompletionType.BASIC);
+        List<String> lookupElementStrings = myFixture.getLookupElementStrings();
+        assertEmpty(lookupElementStrings);
+        tearDown();
+    }
+}
