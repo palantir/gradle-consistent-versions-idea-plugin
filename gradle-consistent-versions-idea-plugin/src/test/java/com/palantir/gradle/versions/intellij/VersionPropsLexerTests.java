81da3d69f3dbe8c992d383e0ce4a3cd4c033afe8
Finlay Williams <finlayw@palantir.com>
2024-10-07 16:34:23 +0100

    fail if unexpected tests

--- gradle-consistent-versions-idea-plugin/src/test/java/com/palantir/gradle/versions/intellij/VersionPropsLexerTests.java
+++ gradle-consistent-versions-idea-plugin/src/test/java/com/palantir/gradle/versions/intellij/VersionPropsLexerTests.java
@@ -27,19 +27,24 @@ import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase5;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
-import org.junit.jupiter.api.BeforeAll;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class VersionPropsLexerTests extends LightJavaCodeInsightFixtureTestCase5 {
+
+    private static String basePath = "src/test/resources/lexerTests/";
 
     @Override
     protected final String getRelativePath() {
@@ -51,36 +56,10 @@ public class VersionPropsLexerTests extends LightJavaCodeInsightFixtureTestCase5
         return "";
     }
 
-    @BeforeAll
-    public static void before() throws IOException {
-        Optional<String> inCi = Optional.ofNullable(System.getenv("CI"));
-
-        if (!inCi.equals(Optional.of("true"))) {
-            Path directoryToDelete = Paths.get("src/test/resources/lexerTests");
-            if (Files.exists(directoryToDelete)) {
-                deleteDirectoryRecursively(directoryToDelete);
-            }
-        }
-    }
-
-    private static void deleteDirectoryRecursively(Path path) throws IOException {
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
-            for (Path entry : stream) {
-                if (Files.isDirectory(entry)) {
-                    deleteDirectoryRecursively(entry);
-                } else {
-                    Files.delete(entry);
-                }
-            }
-        }
-        Files.delete(path);
-    }
-
-    @ValueSource(
-            strings = {
+    private static Stream<Set<String>> provideTestCases() {
+        return Stream.of(Set.of(
                 "a.normal:example = 1",
                 "random.characters:after = version blah blah",
-                "a.normal:example = 1",
                 "has.end.of:line=comments#here is the comment",
                 "odd/.characters@?'.in!:all\\\\/ = the_pl4ces.,?",
                 "has.end.of:line=comments # here is the comment",
@@ -90,25 +69,76 @@ public class VersionPropsLexerTests extends LightJavaCodeInsightFixtureTestCase5
                 "has.spaces.around : colon = 1",
                 "has.no.spaces.around:equals=1",
                 "has.loads.of.spaces:around   =   equals",
-                "    # comment after spaces"
-            })
+                "    # comment after spaces"));
+    }
+
     @ParameterizedTest
-    public void test_psi_tree_structure(String input) throws Exception {
+    @MethodSource("provideTestCases")
+    public void test_psi_tree_structure(Set<String> input) throws Exception {
         JavaCodeInsightTestFixture fixture = getFixture();
-        fixture.configureByText("versions.props", input);
-
         Optional<String> inCi = Optional.ofNullable(System.getenv("CI"));
-        ApplicationManager.getApplication().runReadAction(() -> {
-            PsiFile psiFile = fixture.getFile();
-            String tokenString = convertPsiToString(psiFile);
 
-            if (inCi.equals(Optional.of("true"))) {
-                String expectedTokens = loadExpectedTokens(input);
-                assertThat(input + "\n" + tokenString).isEqualTo(expectedTokens);
-            } else {
-                saveActualTokens(input, tokenString);
-            }
+        // If we are running on remote check all the expected test files are checked in else clean the test directory
+        if (inCi.equals(Optional.of("true"))) {
+            check_tests_match_input(input);
+        } else {
+            deleteOldTests(Paths.get(basePath));
+        }
+        input.forEach(inputItem -> {
+            fixture.configureByText("versions.props", inputItem);
+            ApplicationManager.getApplication().runReadAction(() -> {
+                PsiFile psiFile = fixture.getFile();
+                String tokenString = convertPsiToString(psiFile);
+
+                if (inCi.equals(Optional.of("true"))) {
+                    String expectedTokens = loadExpectedTokens(inputItem);
+                    assertThat(inputItem + "\n" + tokenString).isEqualTo(expectedTokens);
+                } else {
+                    saveActualTokens(inputItem, tokenString);
+                }
+            });
         });
+    }
+
+    private void check_tests_match_input(Set<String> input) {
+        File baseDir = new File(basePath);
+        File[] allFiles = baseDir.listFiles();
+
+        if (allFiles == null) {
+            throw new RuntimeException("Base directory does not exist or is not a directory.");
+        }
+
+        Set<String> inputFileNames = input.stream().map(this::hashToFolderName).collect(Collectors.toSet());
+
+        Set<String> baseDirFileNames =
+                Arrays.stream(allFiles).map(File::getName).collect(Collectors.toSet());
+
+        for (String fileName : inputFileNames) {
+            assertThat(baseDirFileNames)
+                    .describedAs(
+                            "Expected file %s does not exist in the base directory. Run tests locally to fix.",
+                            fileName)
+                    .contains(fileName);
+        }
+
+        for (String fileName : baseDirFileNames) {
+            assertThat(inputFileNames)
+                    .describedAs("Unexpected file %s exists in the base directory. Run tests locally to fix.", fileName)
+                    .contains(fileName);
+        }
+    }
+
+    private static void deleteOldTests(Path path) throws IOException {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+            for (Path entry : stream) {
+                if (Files.isDirectory(entry)) {
+                    deleteOldTests(entry);
+                    Files.delete(entry);
+                } else {
+                    Files.delete(entry);
+                }
+            }
+        }
     }
 
     private String convertPsiToString(PsiFile psiFile) {
@@ -143,14 +173,14 @@ public class VersionPropsLexerTests extends LightJavaCodeInsightFixtureTestCase5
         }
     }
 
-    private static Path getOutputFilePath(String input) throws IOException {
+    private Path getOutputFilePath(String input) throws IOException {
         String fileName = hashToFolderName(input);
-        Path outputPath = Paths.get("src/test/resources/lexerTests/" + fileName);
+        Path outputPath = Paths.get(basePath + fileName);
         Files.createDirectories(outputPath.getParent());
         return outputPath;
     }
 
-    private static String hashToFolderName(String input) {
+    private String hashToFolderName(String input) {
         return Hashing.sha256().hashString(input, StandardCharsets.UTF_8).toString();
     }
 }
