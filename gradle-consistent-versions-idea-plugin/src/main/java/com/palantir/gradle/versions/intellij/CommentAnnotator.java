e754aeedca91ccd4b293ec0a1817ac9c2fa93b0c
Finlay Williams <finlayw@palantir.com>
2024-10-09 13:10:06 +0100

    highlighting

---
+++ gradle-consistent-versions-idea-plugin/src/main/java/com/palantir/gradle/versions/intellij/CommentAnnotator.java
@@ -0,0 +1,73 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.gradle.versions.intellij;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.editor.markup.TextAttributes;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiComment;
+import com.intellij.psi.PsiElement;
+import com.intellij.ui.JBColor;
+import java.awt.Font;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CommentAnnotator implements Annotator {
+
+    private static final TextAttributesKey RED_BOLD = TextAttributesKey.createTextAttributesKey(
+            "DEPENDENCY_UPGRADER_OFF", new TextAttributes(JBColor.RED, null, null, null, Font.BOLD));
+
+    private static final TextAttributesKey GREEN_BOLD = TextAttributesKey.createTextAttributesKey(
+            "DEPENDENCY_UPGRADER_ON", new TextAttributes(JBColor.GREEN, null, null, null, Font.BOLD));
+
+    private static final Pattern DEPENDENCY_UPGRADER_OFF_PATTERN =
+            Pattern.compile("dependency-upgrader:OFF", Pattern.CASE_INSENSITIVE);
+    private static final Pattern DEPENDENCY_UPGRADER_ON_PATTERN =
+            Pattern.compile("dependency-upgrader:ON", Pattern.CASE_INSENSITIVE);
+
+    @Override
+    public void annotate(PsiElement element, AnnotationHolder holder) {
+        if (element instanceof PsiComment) {
+            String commentText = element.getText();
+
+            Matcher offMatcher = DEPENDENCY_UPGRADER_OFF_PATTERN.matcher(commentText);
+            if (offMatcher.find()) {
+                int startOffset = element.getTextRange().getStartOffset() + offMatcher.start();
+                int endOffset = startOffset + offMatcher.group().length();
+                TextRange textRange = new TextRange(startOffset, endOffset);
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                        .range(textRange)
+                        .textAttributes(RED_BOLD)
+                        .create();
+            } else {
+                Matcher onMatcher = DEPENDENCY_UPGRADER_ON_PATTERN.matcher(commentText);
+                if (onMatcher.find()) {
+                    int startOffset = element.getTextRange().getStartOffset() + onMatcher.start();
+                    int endOffset = startOffset + onMatcher.group().length();
+                    TextRange textRange = new TextRange(startOffset, endOffset);
+                    holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                            .range(textRange)
+                            .textAttributes(GREEN_BOLD)
+                            .create();
+                }
+            }
+        }
+    }
+}
