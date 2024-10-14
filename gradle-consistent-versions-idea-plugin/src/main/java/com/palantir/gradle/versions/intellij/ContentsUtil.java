14ed3935191b2459b4d7383c56d2078413f24c8a
Finlay Williams <finlayw@palantir.com>
2024-10-14 10:56:01 +0100

    better logging

--- gradle-consistent-versions-idea-plugin/src/main/java/com/palantir/gradle/versions/intellij/ContentsUtil.java
+++ gradle-consistent-versions-idea-plugin/src/main/java/com/palantir/gradle/versions/intellij/ContentsUtil.java
@@ -22,6 +22,7 @@ import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.ProgressManager;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -57,29 +58,37 @@ public final class ContentsUtil {
 
     private static Callable<String> fetchContentTask(URL pageUrl, ProgressIndicator indicator) {
         return () -> {
-            HttpURLConnection connection = (HttpURLConnection) pageUrl.openConnection();
-            connection.setRequestMethod("GET");
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) pageUrl.openConnection();
+                connection.setRequestMethod("GET");
 
-            if (connection.getResponseCode() != 200) {
-                connection.disconnect();
-                throw new ProcessCanceledException();
-            }
-
-            BufferedReader in =
-                    new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-            StringBuilder result = new StringBuilder();
-            String inputLine;
-
-            while ((inputLine = in.readLine()) != null) {
-                if (indicator.isCanceled()) {
-                    throw new InterruptedException("Fetch cancelled");
+                if (connection.getResponseCode() != 200) {
+                    throw new ProcessCanceledException();
                 }
-                result.append(inputLine);
-            }
 
-            in.close();
-            connection.disconnect();
-            return result.toString();
+                BufferedReader in =
+                        new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+                StringBuilder result = new StringBuilder();
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    if (indicator.isCanceled()) {
+                        throw new InterruptedException("Fetch cancelled");
+                    }
+                    result.append(inputLine);
+                }
+
+                in.close();
+                return result.toString();
+            } catch (ConnectException e) {
+                log.error("Connection refused on page {}", pageUrl, e);
+                throw e;
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
         };
     }
 
