a3cb44abffa90fb4c690defe93ac4de36cbf9ea5
Finlay Williams <finlayw@palantir.com>
2025-03-11 13:19:05 +0000

    registar dynamic

--- gradle-consistent-versions-idea-plugin/src/main/java/com/palantir/gradle/versions/intellij/DebouncingAsyncFileListener.java
+++ gradle-consistent-versions-idea-plugin/src/main/java/com/palantir/gradle/versions/intellij/DebouncingAsyncFileListener.java
@@ -1,30 +1,18 @@
 /*
- * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * (c) Copyright 2024 Palantir Technologies Inc.
+ * Licensed under the Apache License, Version 2.0.
  */
-
 package com.palantir.gradle.versions.intellij;
 
-import com.intellij.openapi.Disposable;
+import com.intellij.concurrency.JobScheduler;
 import com.intellij.openapi.vfs.AsyncFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
-import com.intellij.util.Alarm;
-import com.intellij.util.SingleAlarm;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,13 +21,14 @@ public final class DebouncingAsyncFileListener implements AsyncFileListener {
     private static final Logger log = LoggerFactory.getLogger(DebouncingAsyncFileListener.class);
 
     private final AsyncFileListener delegate;
-    private final SingleAlarm alarm;
     private final BlockingQueue<VFileEvent> bufferedEvents = new LinkedBlockingQueue<>();
 
-    DebouncingAsyncFileListener(AsyncFileListener delegate, int debounceDelayMillis, Disposable parentDisposable) {
+    private final int debounceDelayMillis;
+    private volatile Future<?> scheduledFuture;
+
+    DebouncingAsyncFileListener(AsyncFileListener delegate, int debounceDelayMillis) {
         this.delegate = delegate;
-        this.alarm = new SingleAlarm(
-                this::processEvents, debounceDelayMillis, parentDisposable, Alarm.ThreadToUse.POOLED_THREAD);
+        this.debounceDelayMillis = debounceDelayMillis;
     }
 
     @SuppressWarnings("for-rollout:Slf4jLogsafeArgs")
@@ -48,8 +37,16 @@ public final class DebouncingAsyncFileListener implements AsyncFileListener {
     public ChangeApplier prepareChange(List<? extends VFileEvent> events) {
         log.debug("Received events: {}", events);
         bufferedEvents.addAll(events);
-        alarm.request();
+        scheduleDebouncedProcessing();
         return null;
+    }
+
+    private synchronized void scheduleDebouncedProcessing() {
+        if (scheduledFuture != null && !scheduledFuture.isDone()) {
+            scheduledFuture.cancel(false);
+        }
+        scheduledFuture =
+                JobScheduler.getScheduler().schedule(this::processEvents, debounceDelayMillis, TimeUnit.MILLISECONDS);
     }
 
     @SuppressWarnings("for-rollout:Slf4jLogsafeArgs")
--- gradle-consistent-versions-idea-plugin/src/main/java/com/palantir/gradle/versions/intellij/VersionPropsListenerRegistrar.java
+++ gradle-consistent-versions-idea-plugin/src/main/java/com/palantir/gradle/versions/intellij/VersionPropsListenerRegistrar.java
@@ -1,35 +1,23 @@
 /*
- * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * (c) Copyright 2024 Palantir Technologies Inc.
+ * Licensed under the Apache License, Version 2.0.
  */
 
 package com.palantir.gradle.versions.intellij;
 
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.vfs.AsyncFileListener;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import java.util.List;
 import org.jetbrains.annotations.Nullable;
 
-public final class VersionPropsListenerRegistrar implements AsyncFileListener, Disposable {
+public final class VersionPropsListenerRegistrar implements AsyncFileListener {
 
     private final FilteringAsyncFileListener changeListener;
 
     VersionPropsListenerRegistrar() {
         this.changeListener = new FilteringAsyncFileListener(
-                new DebouncingAsyncFileListener(new VersionPropsFileListener(), 250, this), this::isRelevantFile);
+                new DebouncingAsyncFileListener(new VersionPropsFileListener(), 250), this::isRelevantFile);
     }
 
     private boolean isRelevantFile(VirtualFile virtualFile) {
@@ -42,7 +30,4 @@ public final class VersionPropsListenerRegistrar implements AsyncFileListener, D
     public ChangeApplier prepareChange(List<? extends VFileEvent> events) {
         return changeListener.prepareChange(events);
     }
-
-    @Override
-    public void dispose() {}
 }
